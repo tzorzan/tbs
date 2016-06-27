65e0f6956f347a09f935d79b78695e577592778a
Tobia Zorzan <tobia.zorzan@gmail.com>
2016-06-27 10:25:23 +0200

    Externalised configuration for timeout and unknown name.

--- src/main/java/it/tzorzan/Actions.java
+++ src/main/java/it/tzorzan/Actions.java
@@ -2,9 +2,11 @@ package it.tzorzan;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +15,24 @@ import java.util.TimerTask;
 
 import static it.tzorzan.Variables.*;
 
+@Component
 public class Actions {
     private static final Logger log = LoggerFactory
             .getLogger(TbsApplication.class);
 
-    public static final Integer TIMEOUT = 30000;
-    public static final String UNKNOWN = "John Doe";
+    public static Integer TIMEOUT;
+
+    @Value("${tbs.queue.timeout}")
+    public void setQueueTimeout(Integer timeout) {
+        TIMEOUT = timeout;
+    }
+
+    public static String UNKNOWN;
+
+    @Value("${tbs.queue.unknown}")
+    public void setQueueUnknown(String unknown) {
+        UNKNOWN = unknown;
+    }
 
     @Bean
     public static Action<States, Events> initvar() {
